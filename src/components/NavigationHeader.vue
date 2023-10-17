0114a24e8f882eb3991cac04da9aa6bcaf3bf4b0
T.M <ace071144@gmail.com>
2023-10-17 21:07:16 +0900

    メニュー切り出し

--- src/components/NavigationHeader.vue
+++ src/components/NavigationHeader.vue
@@ -3,18 +3,7 @@
     <nav class="menu-wrap">
       <section class="flex-contents">
         <section class="service-icon">サービスアイコン</section>
-        <section class="menu">
-          <ul class="menu-item">
-            <li v-if="!auth0.isAuthenticated.value"><button @click="login">ログイン</button></li>
-            <li v-if="auth0.isAuthenticated.value"><a class="sp-char-mini" href="/">Profile</a></li>
-            <li v-if="auth0.isAuthenticated.value">
-              <router-link :to="{ name: 'libraries', params: { username: String(auth0.user.value.nickname) } }">
-                登録済み<br>ライブラリ
-              </router-link>
-            </li>
-            <li v-if="auth0.isAuthenticated.value"><button @click="logout">ログアウト</button></li>
-          </ul>
-        </section>
+        <MenuItem />
       </section>
     </nav>
   </header>
@@ -23,24 +12,14 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { useAuth0 } from '@auth0/auth0-vue';
+import MenuItem from "@/components/MenuItem.vue";
 
 export default defineComponent({
   name: 'Login',
-  components: {},
-  setup() {
-    const auth0 = useAuth0();
-    const { logout } = useAuth0();
-
-    return {
-      auth0,
-      login: () => {
-        auth0.loginWithRedirect()
-      },
-      logout: () => {
-        logout({ logoutParams: { returnTo: `${import.meta.env.VITE_BASE_URL}` } });
-      }
-    };
-  }
+  components: {
+    MenuItem,
+  },
+  setup() {}
 });
 </script>
 
@@ -59,26 +38,5 @@ nav {
 
 .menu-wrap {
   padding: 0.2em;
-}
-.menu {
-  display: flex;
-}
-.menu li {
-  padding: 0.5em 2em;
-  font-size: 1.0rem;
-  font-weight: 700;
-}
-.menu-item {
-  display: flex;
-  justify-content: center;
-}
-
-.search-wrap {
-  position: relative;
-  display: flex;
-}
-.searchbox {
-  position: absolute;
-  top: 30%;
 }
 </style>
