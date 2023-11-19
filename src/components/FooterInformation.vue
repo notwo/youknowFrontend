aae1fc7d1780391b0a132a741dbca2a9f1ad554c
T.M <ace071144@gmail.com>
2023-11-19 23:26:37 +0900

    footerのcss修正

--- src/components/FooterInformation.vue
+++ src/components/FooterInformation.vue
@@ -1,27 +1,58 @@
-<template>
-  <footer>
-    <article>
-      <section class="service-logo">サービスロゴ</section>
-      <section class="copyright">copyright</section>
-    </article>
-  </footer>
-</template>
-
-<script>
-/*
-import { defineComponent } from '@vue/composition-api'
-
-export default defineComponent({
-  setup() {
-    
-  },
-})
-*/
+<script set lang="ts">
 </script>
 
-<style>
+<style scoped>
 footer {
+  padding: 3rem 0 0;
   background-color: black;
   color: white;
 }
+
+.footer-contents-wrap {
+  max-width: 1100px;
+  margin: 0 auto;
+}
+.footer-contents-subject {
+  font-weight: bold;
+}
+.footer-contents-list {
+  margin: .8rem 0;
+}
+
+.copyright {
+  margin: .5rem 0;
+  font-size: .78rem;
+  text-align: center;
+}
 </style>
+
+<template>
+  <footer>
+    <section class="footer-contents-wrap flex-contents">
+      <section class="footer-contents">
+        <section class="footer-contents-logo">サービスロゴ</section>
+      </section>
+      <section class="footer-contents">
+        <nav>
+          <section class="footer-contents-subject">YouKnowについて</section>
+          <ul>
+            <li class="footer-contents-list">YouKnowの使い方</li>
+            <li class="footer-contents-list">リリース</li>
+          </ul>
+        </nav>
+      </section>
+      <section class="footer-contents">
+        <nav>
+          <section class="footer-contents-subject">諸情報</section>
+          <ul>
+            <li class="footer-contents-list">プライバシーポリシー</li>
+            <li class="footer-contents-list">利用規約</li>
+          </ul>
+        </nav>
+      </section>
+    </section>
+    <section class="copyright">
+      © 2023 YouKnow
+    </section>
+  </footer>
+</template>
