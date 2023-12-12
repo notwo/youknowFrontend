7bc740452c2dd669786e6a88be5cb5c4c721d62a
T.M <ace071144@gmail.com>
2023-12-12 12:19:53 +0900

    コメントアウト削除

--- src/components/views/Top.vue
+++ src/components/views/Top.vue
@@ -1,66 +1,8 @@
-<template>
-  <h3>TOP</h3>
-</template>
-
-<script lang="ts">
-import { defineComponent } from 'vue';
-import axios, { AxiosResponse, AxiosError } from "axios";
-import { useAuth0 } from '@auth0/auth0-vue';
-import { userApi } from '@/plugin/apis';
-
-export default defineComponent({
-  name: 'Top',
-  setup() {
-    /*
-    const { user, isAuthenticated, logout } = useAuth0();
-    if (isAuthenticated.value) {
-      interface UserRequest {
-        sub: String,
-        username: String,
-        email: String
-      };
-      interface UserResponse {
-        data: [],
-      };
-
-      interface ErrorResponse {
-        message: String,
-        name: String,
-        code: String
-      };
-
-      const api = userApi();
-      const registerUser = async () => {
-        const requestParam: UserRequest = {
-          sub: user.value.sub,
-          username: user.value.nickname,
-          email: user.value.email,
-          //email_verified: user.value.email_verified,
-          //picture: user.value.picture,
-        };
-        axios.post(api.createUrl(), requestParam)
-          .then((response: AxiosResponse) => {
-          })
-          .catch((e: AxiosError<ErrorResponse>) => {
-            // 何かしらエラーが起きてアカウント登録できていないため、アラート表記の上ログアウトさせる
-            logout();
-            alert('アカウント登録時に異常が発生しました。お手数ですが、しばらく経ってから再度お試しください。');
-          });
-      };
-
-      const registerOrUpdate = async () => {
-        const response = await axios.get<UserResponse>(api.detailUrl(user.value.sub));
-        if (response.data.length === 0) {
-          registerUser();
-        }
-      };
-
-      registerOrUpdate();
-    }
-    */
-  },
-});
-</script>
+<script setup lang="ts"></script>
 
 <style scoped>
 </style>
+
+<template>
+  <h3>TOP</h3>
+</template>
