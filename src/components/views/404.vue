2c7d74061bed90b58466da294e10392c7d5c7e3c
T.M <ace071144@gmail.com>
2023-12-12 18:56:01 +0900

    404の文言修正

--- src/components/views/404.vue
+++ src/components/views/404.vue
@@ -1,6 +1,39 @@
 <script setup lang="ts">
 </script>
 
+<style scoped>
+.error-message {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  line-height: 1.9rem;
+  font-size: 1.2rem;
+}
+
+.error-message p {
+  text-align: center;
+}
+
+.to-top {
+  display: flex;
+  justify-content: center;
+}
+
+.to-top a {
+  color: #444;
+  text-decoration: none;
+}
+</style>
+
 <template>
-  <section>404 Not Found</section>
+  <!-- ここにYouKnowの404画像を載せたい -->
+  <section class="error-message">
+    <p>お探しのページが見つかりませんでした。<br>
+      入力されたURLが間違っているか、データが既に存在しない可能性があります。</p>
+  </section>
+  <section class="to-top">
+    <router-link :to="{ name: 'top' }">
+      You Know TOPへ戻る
+    </router-link>
+  </section>
 </template>
