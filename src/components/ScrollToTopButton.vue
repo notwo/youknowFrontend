338706c0f3db44fb55673724ce3d83cc8fa1831f
T.M <ace071144@gmail.com>
2023-12-22 07:56:24 +0900

    scroll button

--- src/components/ScrollToTopButton.vue
+++ src/components/ScrollToTopButton.vue
@@ -86,6 +86,14 @@ onUnmounted(() => {
     opacity: 0;
   }
 }
+
+/* sp, tablet */
+@media screen and (max-width: 768px) {
+  .scroll-to-top-button {
+    bottom: .3rem;
+    right: .3rem;
+  }
+}
 </style>
 
 <template>
