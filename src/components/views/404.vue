33618cdfa42eb42dbf43dafe8ab4f2676908d996
T.M <ace071144@gmail.com>
2023-12-22 09:00:45 +0900

    404

--- src/components/views/404.vue
+++ src/components/views/404.vue
@@ -23,10 +23,23 @@
   color: #444;
   text-decoration: none;
 }
+
+/* sp */
+@media screen and (max-width: 768px) {
+  .error-message {
+    margin: 2rem;
+    font-size: 1rem;
+  }
+
+  .to-top a {
+    font-size: 1.2rem;
+    font-weight: 600;
+  }
+}
 </style>
 
 <template>
-  <!-- ここにYouKnowの404画像を載せたい -->
+  <!-- TODO: ここにYouKnowの404画像を載せたい -->
   <section class="error-message">
     <p>お探しのページが見つかりませんでした。<br>
       入力されたURLが間違っているか、データが既に存在しない可能性があります。</p>
