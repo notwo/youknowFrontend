303d2392b0a733b49c01113720def5df7572b7de
T.M <ace071144@gmail.com>
2023-12-27 13:11:11 +0900

    不要なコメント削除

--- src/components/NavigationHeader.vue
+++ src/components/NavigationHeader.vue
@@ -1,25 +1,5 @@
 <script setup lang="ts">
 import MenuItem from "@/components/MenuItem.vue";
-
-/*
-const nav = document.getElementsByTagName('nav');
-const navigationTracking = () => {
-  const height = nav[0].clientHeight;
-  const navOffsetTop = window.scrollY;
-  if (navOffsetTop >= height / 2) {
-    nav[0].classList.add('fixed');
-  } else {
-    nav[0].classList.remove('fixed');
-  }
-};
-onMounted(() => {
-  window.addEventListener("scroll", navigationTracking, false);
-});
-
-onUnmounted(() => {
-  window.removeEventListener('scroll', navigationTracking, false);
-});
-*/
 </script>
 
 <style scoped>
